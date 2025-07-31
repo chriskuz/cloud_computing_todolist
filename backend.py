a16cae39fb693655b6fbf884f3f32fa0c2ad9d22
Chris Kuzemka <cmkuzemka@gmail.com>
2025-07-30 23:03:39 -0400

    started creating a deployment yaml file. adjusted ports to run on 80. made the code a little more modular.

--- backend.py
+++ backend.py
@@ -78,6 +78,6 @@ def close_db(error):
 
 ## Program Instantiation
 if __name__ == "__main__":
-    app.run("0.0.0.0", port=5002, debug=True)
+    app.run("0.0.0.0", port=80, debug=True)
     # app.run("0.0.0.0", port=80)
     # app.run("0.0.0.0", port=5000)
--- frontend.py
+++ frontend.py
@@ -9,15 +9,14 @@ import os
 app = Flask(__name__) #always needed; makes app work. config method removed as unecessary
 
 ## Functions and Environment
-BACKEND_BASE_URL = os.getenv("BACKEND_BASE_URL", "http://localhost:5001")
+backend_port = 80
 
-back_end_port = 5002
-
+BACKEND_BASE_URL = os.getenv("BACKEND_BASE_URL", f"http://localhost:{backend_port}") #first variable is the name of the environment variable we're trying to assign something to while the 2nd value is what we default that variable to. 
 
 # Root (REFACTORED)
 @app.route("/")
 def show_list():
-      resp = requests.get(f"http://localhost:{back_end_port}/api/items") #this may need to be changed
+      resp = requests.get(f"{BACKEND_BASE_URL}/api/items") #this may need to be changed
       resp = resp.json()
       return render_template('index.html', todolist=resp)
 
@@ -30,7 +29,7 @@ def add_entry():
         "due_date": request.form["due_date"],
         "status": request.form.get("status", "incomplete")
     }
-    requests.post(f"http://localhost:{back_end_port}/api/add", json=data)
+    requests.post(f"{BACKEND_BASE_URL}/api/add", json=data)
     return redirect(url_for('show_list')) #this comomand is important
 
 
@@ -38,7 +37,7 @@ def add_entry():
 @app.route("/delete/<item>")
 def delete_entry(item):
     data = {"what_to_do": item}
-    requests.post(f"http://localhost:{back_end_port}/api/delete", json=data)
+    requests.post(f"{BACKEND_BASE_URL}/api/delete", json=data)
     return redirect(url_for('show_list'))
 
 
@@ -46,12 +45,12 @@ def delete_entry(item):
 @app.route("/mark/<item>")
 def mark_as_done(item):
     data = {"what_to_do": item}
-    requests.post(f"http://localhost:{back_end_port}/api/mark_as_done", json=data)
+    requests.post(f"{BACKEND_BASE_URL}/api/mark_as_done", json=data)
     return redirect(url_for('show_list'))
 
 
 ## Program Instantiation
 if __name__ == "__main__":
-    app.run("0.0.0.0", port=5003, debug=True)
+    app.run("0.0.0.0", port=backend_port, debug=True)
     # app.run("0.0.0.0", port=80)
     # app.run("0.0.0.0", port=5000)
